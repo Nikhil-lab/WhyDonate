12e1cd98b75c38b6c435e55861bd59a1ac8778da
nikhil-lab <nikhil.m2395@gmail.com>
2020-11-29 18:07:29 +0530

    validation correction

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 import { ApiService } from '../api.service';
 
 @Component({
@@ -9,26 +9,35 @@ import { ApiService } from '../api.service';
 })
 export class CustomerComponent implements OnInit {
 
-  name = new FormControl('', [Validators.required]);
-  email = new FormControl('', [Validators.required, Validators.email]);
-  phone = new FormControl('', [Validators.required]);
-  address = new FormControl('', [Validators.required]);
+  customerForm=new FormGroup ({
+    name :new FormControl('', [Validators.required]),
+  email : new FormControl('', [Validators.required, Validators.email]),
+  phone : new FormControl('', [Validators.required]),
+  address : new FormControl('', [Validators.required]),
+  });
+
+  
 
   constructor(private api:ApiService) { }
 
   getErrorMessage() {
-    if (this.email.hasError('required')) {
+    if (this.customerForm.controls['name'].hasError('required')) {
       return 'You must enter a value';
     }
+    else{   
+      if (this.customerForm.controls['name'].hasError('email')) {
+        return 'not a valid email';
+      }
+    }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+    
   }
 
   ngOnInit(): void {
   }
 
-  onSubmit(value){
-    console.log(value);
+  onSubmit(){
+    console.log(this.customerForm.value);
     this.api.getData().subscribe(data=>{
       console.log("received data from server");
       console.log(data);
